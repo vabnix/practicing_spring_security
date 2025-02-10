e5a5580bfded764495d269af4ad6b2445ff7b668
Vaibhav Srivastava <vabnix@hotmail.com>
2025-02-10 11:04:14 -0600

    adding ability to store the user in database

--- src/main/java/com/vaibhav/security/configuration/SecurityConfig.java
+++ src/main/java/com/vaibhav/security/configuration/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.vaibhav.security.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -11,12 +12,18 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
+
+    @Autowired
+    DataSource dataSource;
 
     @Bean
     SecurityFilterChain customSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -43,6 +50,21 @@ public class SecurityConfig {
                 .roles("ADMIN")
                 .build();
 
-        return new InMemoryUserDetailsManager(user1, admin);
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        createUserIfNotExists(jdbcUserDetailsManager, "user1", "password", "USER");
+        createUserIfNotExists(jdbcUserDetailsManager, "admin", "password", "ADMIN");
+
+        return jdbcUserDetailsManager;
+    }
+
+    private void createUserIfNotExists(JdbcUserDetailsManager manager, String username,
+                                       String password, String role) {
+        if (!manager.userExists(username)) {
+            UserDetails user = User.withUsername(username)
+                    .password("{noop}" + password)
+                    .roles(role)
+                    .build();
+            manager.createUser(user);
+        }
     }
 }
